1cb6217d563bb219c508640506ebeaacbb2fac61
gopi selvam <selvamgopirgs@gmail.com>
2022-07-09 13:43:23 +0530

    Match API backend

--- ipl-dashboard/src/main/java/io/springreact/ipldashboard/controller/TeamController.java
+++ ipl-dashboard/src/main/java/io/springreact/ipldashboard/controller/TeamController.java
@@ -3,8 +3,13 @@ package io.springreact.ipldashboard.controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import io.springreact.ipldashboard.model.Match;
 import io.springreact.ipldashboard.model.Team;
 import io.springreact.ipldashboard.repository.MatchRepository;
 import io.springreact.ipldashboard.repository.TeamRepository;
@@ -27,6 +32,16 @@ public class TeamController {
 
         team.setMatches(this.matchRepository.findLatestMatchesbyTeam(teamName, 4));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        // return
+        // this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(
+        // teamName, startDate, endDate, teamName, startDate, endDate);
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
     }
 
 }
